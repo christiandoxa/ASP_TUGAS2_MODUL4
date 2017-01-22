7f72c4176985c40656b90194225546b492cfeea4
Christian Doxa Hamasiah <zhergiuz@gmail.com>
2017-01-22 20:55:11 +0700

    - Penambahan controller - Edit View - Adding css - Finishing - Add Screenshot

---
+++ ASP_TUGAS2_MODUL1/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using ASP_TUGAS2_MODUL1.Models.EntityManager;
+using ASP_TUGAS2_MODUL1.Models.ViewModel;
+
+namespace ASP_TUGAS2_MODUL1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login Name already taken.");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ ASP_TUGAS2_MODUL1/Models/EntityManager/UserManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using ASP_TUGAS2_MODUL1.Models.DB;
+using ASP_TUGAS2_MODUL1.Models.ViewModel;
+
+namespace ASP_TUGAS2_MODUL1.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void AddUserAccount(UserSignUpView user)
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                SYSUSER SU = new SYSUSER();
+                SU.LoginName = user.LoginName;
+                SU.PasswordEncryptedText = user.Password;
+                SU.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SU.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SU.RowCreatedDateTime = DateTime.Now;
+                SU.RowModifiedDateTime = DateTime.Now;
+
+                db.SYSUSER.Add(SU);
+                db.SaveChanges();
+
+                SYSUserProfile SUP = new SYSUserProfile();
+                SUP.SYSUserID = SU.SYSUserID;
+                SUP.FirstName = user.FirstName;
+                SUP.LastName = user.LastName;
+                SUP.Gender = user.Gender;
+                SUP.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                SUP.RowCreatedDateTime = DateTime.Now;
+                SUP.RowModifiedDateTime = DateTime.Now;
+
+                db.SYSUserProfile.Add(SUP);
+                db.SaveChanges();
+
+                if (user.LOOKUPRoleID > 0)
+                {
+                    SYSUserRole SUR = new SYSUserRole();
+                    SUR.LOOKUPRoleID = user.LOOKUPRoleID;
+                    SUR.SYSUserID = user.SYSUserID;
+                    SUR.IsActive = true;
+                    SUR.RowCreatedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowModifiedSYSUserID = user.SYSUserID > 0 ? user.SYSUserID : 1;
+                    SUR.RowCreatedDateTime = DateTime.Now;
+                    SUR.RowModifiedDateTime = DateTime.Now;
+
+                    db.SYSUserRole.Add(SUR);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public bool IsLoginNameExist(string loginName)
+        {
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                return db.SYSUSER.Any(o => o.LoginName.Equals(loginName));
+            }
+        }
+    }
+}
